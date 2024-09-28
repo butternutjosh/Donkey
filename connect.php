fd345e94a768e208bf228b50a9e65bc3f44ae164
butternutjosh <121814161+butternutjosh@users.noreply.github.com>
2024-09-28 12:23:57 +0100

    added lots, php still not working

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+$username=$_POST["username"];
+$email=$_POST["email"];
+$password=$_POST["password"];
+$password2=$_POST["password2"];
+var_dump($username,$email, $password, $password2);
+echo $_POST["username"];

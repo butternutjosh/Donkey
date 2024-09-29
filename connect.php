22db37925f44cb5d542eca4ffd764e29e5bf3537
butternutjosh <121814161+butternutjosh@users.noreply.github.com>
2024-09-29 07:18:03 +0100

    re doing everything

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$username=$_POST["username"];
-$email=$_POST["email"];
-$password=$_POST["password"];
-$password2=$_POST["password2"];
-var_dump($username,$email, $password, $password2);
-echo $_POST["username"];
